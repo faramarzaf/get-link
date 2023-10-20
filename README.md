ef6a6fbfb270ad5ad3b6e29b169ccdf3d667ccea
faramarzaf <faramarzafzali@gmail.com>
2023-10-20 22:59:40 +0330

    added new functionalities

--- README.md
+++ README.md
@@ -3,7 +3,3 @@
 Just a scraper for collecting links of DonyayeSerial as a hobby.
 You need to enter the name and quality of a series then you would have files with the links :)
 
-
-Some considerations are needed since this is a playground app:
-- You should enter the name of the series carefully, I mean the exact name that exits in the blog
-- Qualities are now hard-coded, so might not find your desired quality in some cases
--- main.go
+++ main.go
@@ -12,55 +12,56 @@ import (
 )
 
 const (
-	Q_720  = "720p.x265"
-	Q_1080 = "1080p.x265.10bit"
+	BASE_URL = "https://dls2.top-movies2filmha.click/DonyayeSerial/series"
 )
 
 func main() {
 
+	allSeries := getAllSeries()
+
 	fmt.Println("Welcome to get link app.")
 	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Println("Enter the name of series")
-	scanner.Scan()
-	inputName := scanner.Text()
-	seriesName := getSeriesName(inputName)
-
+	for _, seriesName := range allSeries {
+		seasonsCount := getSeasonsCount(seriesName)
+		fmt.Printf("name: %s , seasons: %v\n", seriesName, seasonsCount)
+	}
 	var urls []string
-	var baseUrl string
+	var downloadUrl string
 
 	for {
+		fmt.Println("Enter name: ")
+		scanner.Scan()
+		seriesName := scanner.Text()
 
 		fmt.Println("Enter season: ")
 		scanner.Scan()
 		season, _ := strconv.Atoi(scanner.Text())
 		formattedSeason := fmt.Sprintf("%02d", season)
 
-		fmt.Println("Enter quality: (720 / 1080)")
+		printQualityBySeriesNameAndSeasonNumber(seriesName, formattedSeason)
+		fmt.Println("Enter quality:")
 		scanner.Scan()
 		quality := scanner.Text()
 
-		baseUrl = getBaseUrlByQuality(seriesName, quality, formattedSeason)
+		downloadUrl = getDownloadUrlByQualityAndSeasonNumber(seriesName, quality, formattedSeason)
 
 		c := colly.NewCollector()
-
 		c.OnHTML(".list tr", func(e *colly.HTMLElement) {
 			url := e.ChildAttr("a", "href")
 			if strings.HasSuffix(url, ".mkv") {
-				urls = append(urls, baseUrl+url+"\n")
+				urls = append(urls, downloadUrl+url+"\n")
 			}
 		})
 
 		c.OnError(func(r *colly.Response, err error) {
 			fmt.Println("Request URL:", r.Request.URL, "failed with response:", r.Request, "\nError:", err)
-			os.Exit(-1)
 		})
 
 		c.OnScraped(func(r *colly.Response) {
 		})
 
 		extensions.RandomUserAgent(c)
-		c.Visit(baseUrl)
+		c.Visit(downloadUrl)
 
 		writeToFile("data"+strconv.Itoa(season)+".txt", urls)
 		urls = nil
@@ -76,24 +77,81 @@ func main() {
 
 }
 
-func getSeriesName(inputName string) string {
-	hasSpace := strings.Contains(inputName, " ")
+func getAllSeries() []string {
+	var allSeries []string
+	c := colly.NewCollector()
+	c.OnHTML(".list tr", func(e *colly.HTMLElement) {
+		series := e.ChildAttr("a", "href")
+		formattedName := strings.Replace(series, "/", "", -1)
+		if formattedName != "" && formattedName != ".." { // this is the parent directory column in table of website
+			allSeries = append(allSeries, formattedName)
+		}
+	})
 
-	if hasSpace {
-		return strings.Replace(inputName, " ", ".", -1)
-	}
-	return inputName
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r.Request, "\nError:", err)
+		os.Exit(-1)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+	})
+
+	extensions.RandomUserAgent(c)
+	c.Visit(BASE_URL)
+
+	return allSeries
 }
 
-func getBaseUrlByQuality(seriesName, quality, season string) string {
-	if quality == "720" {
-		return "https://dls2.top-movies2filmha.click/DonyayeSerial/series/" + seriesName + "/Soft.Sub/S" + season + "/" + Q_720 + ".BluRay/"
+func getSeasonsCount(seriesName string) (seasons int) {
+	count := 0
+	seriesHomePageUrl := BASE_URL + "/" + seriesName + "/Soft.Sub/"
 
-	} else if quality == "1080" {
-		return "https://dls2.top-movies2filmha.click/DonyayeSerial/series/" + seriesName + "/Soft.Sub/S" + season + "/" + Q_1080 + ".BluRay/"
-	}
+	//var failedFixedUrlRecords string
 
-	return ""
+	c := colly.NewCollector()
+	c.OnHTML(".list tr", func(e *colly.HTMLElement) {
+		season := e.ChildAttr("a", "href")
+		if strings.HasPrefix(season, "S") {
+			count++
+		}
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r.Request, "\nError:", err)
+		//	failedFixedUrlRecords = BASE_URL + "/" + seriesName + "/"
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+	})
+
+	extensions.RandomUserAgent(c)
+	c.Visit(seriesHomePageUrl)
+
+	return count
+}
+
+func getDownloadUrlByQualityAndSeasonNumber(seriesName, quality, season string) string {
+	return BASE_URL + "/" + seriesName + "/Soft.Sub/S" + season + "/" + quality
+}
+
+func printQualityBySeriesNameAndSeasonNumber(name, formattedSeason string) {
+	var qualities []string
+	c := colly.NewCollector()
+	c.OnHTML(".list tr", func(e *colly.HTMLElement) {
+		quality := e.ChildAttr("a", "href")
+		qualities = append(qualities, quality)
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r.Request, "\nError:", err)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+	})
+
+	extensions.RandomUserAgent(c)
+	c.Visit(BASE_URL + "/" + name + "/" + "Soft.Sub/" + "S" + formattedSeason)
+	fmt.Println(qualities)
 }
 
 func writeToFile(fileName string, data []string) {
